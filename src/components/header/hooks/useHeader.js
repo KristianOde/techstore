02499a7302fa6f0fe2f9e208a4e4448c9c242439
KristianUSN <kristian.odegaard.jobb@gmail.com>
2021-02-13 20:06:21 +0100

    Kommentarer

--- src/components/header/hooks/useHeader.js
+++ src/components/header/hooks/useHeader.js
@@ -9,10 +9,13 @@ const list = [
     "Hjem", "Kategorier", "Tilbud", "Min side", "Kontakt oss", "Logg ut"
 ]
 
+// Oppretter en context
 const ListContext = createContext()
 
+// Custom hook
 export const useHeaderLinks = () => useContext(ListContext)
 
+// Custom provider, som tilbyr context til barnekomponenter
 export function HeaderLinksProvider ({ children }) {
     const [listelements, setListelements] = useState(list)
     return(
