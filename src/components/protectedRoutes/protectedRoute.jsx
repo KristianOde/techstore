4cee55ccf124a85f8ad2a8eb154b66ff0d5a2076
Larssen <46922426+Larssen@users.noreply.github.com>
2021-03-19 01:56:33 +0100

    formatering

--- src/components/protectedRoutes/protectedRoute.jsx
+++ src/components/protectedRoutes/protectedRoute.jsx
@@ -1,23 +1,25 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Navigate
-} from "react-router-dom";
-import firebase from "firebase/app"
+import { BrowserRouter as Router, Route, Navigate } from "react-router-dom";
+import firebase from "firebase/app";
 
-const ProtectedRoute = ({ component: Component, redirectTo, isAuth, path, ...props }) => {
+const ProtectedRoute = ({
+  component: Component,
+  redirectTo,
+  isAuth,
+  path,
+  ...props
+}) => {
   var user = firebase.auth().currentUser;
   if (user) {
-    isAuth = true
+    isAuth = true;
   } else {
-    isAuth = false
+    isAuth = false;
   }
-  
-  if(!isAuth) {
-      return <Navigate to={redirectTo} />;
+
+  if (!isAuth) {
+    return <Navigate to={redirectTo} />;
   }
-  return <Route path={path} element={<Component />} />
+  return <Route path={path} element={<Component />} />;
 };
 
 export default ProtectedRoute;
--- src/components/registrer/registrer.jsx
+++ src/components/registrer/registrer.jsx
@@ -1,53 +1,75 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { useAuth } from '../firebase/context/authContext'
+import { useAuth } from "../firebase/context/authContext";
 import { useNavigate, Link } from "react-router-dom";
-import '../../styles/form.css'
+import "../../styles/form.css";
 
 const Registrer = () => {
   const { register, handleSubmit, errors } = useForm();
-  const [loading, setLoading] = useState(false)
-  const { loggInnFirebase } = useAuth()
-  const navigate = useNavigate()
+  const [loading, setLoading] = useState(false);
+  const { loggInnFirebase } = useAuth();
+  const navigate = useNavigate();
 
   async function onSubmit(e) {
-    let result = await loggInnFirebase(e.brukernavn, e.passord)
+    let result = await loggInnFirebase(e.brukernavn, e.passord);
     if (result.error) {
-      console.log("sadsadsad")
+      console.log("sadsadsad");
     } else {
-      navigate('profil');
+      navigate("profil");
     }
   }
 
-    return (
-      <div className="centerform">
+  return (
+    <div className="centerform">
       <form className="form" onSubmit={handleSubmit(onSubmit)}>
         <h1>Lag bruker</h1>
 
         <label htmlFor="username">Epost</label>
-        <input className="forminput" name="brukernavn" id="brukernavnId" ref={register({ required: true, minLength: 5 })}/>
+        <input
+          className="forminput"
+          name="brukernavn"
+          id="brukernavnId"
+          ref={register({ required: true, minLength: 5 })}
+        />
         {errors.brukernavn && errors.brukernavn.type === "required" && (
-        <p className="formerror">Required</p>)}
+          <p className="formerror">Required</p>
+        )}
         {errors.brukernavn && errors.brukernavn.type === "minLength" && (
-        <p className="formerror">Minimum required length is 5 letters</p>)}
+          <p className="formerror">Minimum required length is 5 letters</p>
+        )}
 
         <label htmlFor="username">brukernavn</label>
-        <input className="forminput" name="brukernavn" id="brukernavnId" ref={register({ required: true, minLength: 5 })}/>
+        <input
+          className="forminput"
+          name="brukernavn"
+          id="brukernavnId"
+          ref={register({ required: true, minLength: 5 })}
+        />
         {errors.brukernavn && errors.brukernavn.type === "required" && (
-        <p className="formerror">Required</p>)}
+          <p className="formerror">Required</p>
+        )}
         {errors.brukernavn && errors.brukernavn.type === "minLength" && (
-        <p className="formerror">Minimum required length is 5 letters</p>)}
+          <p className="formerror">Minimum required length is 5 letters</p>
+        )}
 
         <label htmlFor="password">Passord</label>
-        <input className="forminput" name="passord" id="passordID" ref={register({ required: true })}/>
+        <input
+          className="forminput"
+          name="passord"
+          id="passordID"
+          ref={register({ required: true })}
+        />
         {errors.passord && errors.passord.type === "required" && (
-        <p className="formerror">Required</p>)}
+          <p className="formerror">Required</p>
+        )}
 
-        <input disabled={loading} type="submit" className="formknapp"/>
-        <p class="message">Har allerede en bruker? <Link to="/login">Logg inn</Link></p>
+        <input disabled={loading} type="submit" className="formknapp" />
+        <p class="message">
+          Har allerede en bruker? <Link to="/login">Logg inn</Link>
+        </p>
       </form>
-      </div>
-    );
-}
+    </div>
+  );
+};
 
-export default Registrer
+export default Registrer;
