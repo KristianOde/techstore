60b9f6ef6a4d938f900a183ac275d7a83926f663
Larssen <46922426+Larssen@users.noreply.github.com>
2021-03-19 17:17:50 +0100

    logg ut knapp for å teste noe

--- src/components/profilSide/profilSide.jsx
+++ src/components/profilSide/profilSide.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useAuth } from "../firebase/context/authContext";
+import { useNavigate } from "react-router-dom";
+import "../../styles/form.css";
 
 const ProfilSide = () => {
-  return(
-      <div>
-          Hemmelig profilside (OmO)
-      </div>
-  )
-}
+  const { register, handleSubmit, errors } = useForm();
+  const [loading, setLoading] = useState(false);
+  const { loggUt } = useAuth();
+
+  const navigate = useNavigate();
+
+  async function onSubmit() {
+    await loggUt().then(navigate("/"));
+  }
+
+  return (
+    <div>
+      Logg ut?
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        <input title="Logg Ut" disabled={loading} type="submit" className="formknapp"/>
+      </form>
+    </div>
+  );
+};
 
 export default ProfilSide
