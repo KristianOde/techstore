2347f2eaa9be3fbbb4b45b073f83dff01c079207
KristianUSN <kristian.odegaard.jobb@gmail.com>
2021-03-31 14:52:15 +0200

    Github-link i footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,11 +3,12 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className="Footer">
-            <p>
+            <span style={{padding:"0 20px"}}>
                 The Flaskehals™️, en mock nettbutikk – 
                 Lagd av Kristian og Mikael – 
                 USN
-            </p>
+            </span>
+            <a href="https://github.com/KristianUSN/techstore">GitHub</a>
         </div>
     )
 }
