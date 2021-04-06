2374ea45adfba876f14e8c856cb2fdd2246ecd38
Larssen <46922426+Larssen@users.noreply.github.com>
2021-04-06 21:23:51 +0200

    Update firebase.jsx
    temp

--- src/components/firebase/firebase.jsx
+++ src/components/firebase/firebase.jsx
@@ -3,12 +3,12 @@ import firebase from "firebase/app"
 
 const app = firebase.initializeApp({
     apiKey: "AIzaSyCX7KL9Zf8UP0XWK01gwRnyhPuLOq_UONs",
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID 
+    authDomain: "test-b830c.firebaseapp.com",
+    databaseURL: "https://test-b830c.firebaseio.com",
+    projectId: "test-b830c",
+    storageBucket: "test-b830c.appspot.com",
+    messagingSenderId: "594028390440",
+    appId: "1:594028390440:web:49b7610990c2ca57f0ed1d"
 })
 
 export const auth = app.auth()
